20c747d0cbc5cf8b435cef4b7e7ee7bfd89f96cd
Michael Young <michaelyoung.aero@gmail.com>
2023-07-12 09:29:35 -0400

    quality of life improvements

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ local step = {}
 
 local http_api = minetest.request_http_api()
 local player_metadata = {}
+local error_message = {}
 
 if not http_api then
     print("error", "Failed to get the HTTP API!")
@@ -21,6 +22,7 @@ if not auth_key then
 end
 
 -- Then, when making the HTTP request, include the auth key in the headers
+local login_url = "https://keypmine.luxumbra.dev/"
 local auth_api_url = "https://minetest-auth.luxumbra.dev/"
 local auth_headers = {
     ["Authorization"] = "Bearer ".. auth_key
@@ -29,50 +31,25 @@ local auth_headers = {
 minetest.register_on_joinplayer(function(player)
     local player_name = player:get_player_name()
     player_metadata[player_name] = {}
-    step[player_name] = 1
-    local url = "https://keypmine.luxumbra.dev/ "
-    local formspec = 
-        "formspec_version[4]" ..
-        "size[9,9]" ..
-        "no_prepend[]" ..
-        "background[0,0;9,9;login-modal-welcome-75alpha.png;true]" ..
-        "bgcolor[;neither;]" ..
-        "textarea[2,4.5;5,.5;url;;"..url.."]" ..
-        "image_button[3.72,7.25;1.44,1.12;next-button.png;next;;false;next-button.png]"..
-        "image_button[8,.25;.5,.5;close-button.png;exit;;false;close-button.png]"
-    minetest.show_formspec(player_name, "keyp:login", formspec)
+    minetest.chat_send_player(player_name, "Welcome to the minetest server with Keyp! Type /help for available commands or /login to start the login process if you close it out.")
+    show_initial_login_formspec(player_name)
 end)
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
     if formname == "keyp:login" then
         local player_name = player:get_player_name()
-        if fields.next then
-            step[player_name] = 2
-            local formspec = 
-                "formspec_version[4]" ..
-                "size[9,9]" ..
-                "no_prepend[]" ..
-                "background[0,0;9,9;login-modal-75alpha.png;true]" ..
-                "bgcolor[;neither;]" ..
-                "label[2.75,3;Enter the 6-digit code you received:]" ..
-                "field[3.5,4;1.5,.75;code;Code;]" ..
-                "button[0,0;0,0;submit;]"..
-                "image_button[3.72,7.25;1.44,1.12;next-button.png;submit;;false;next-button.png]"..
-                "image_button[8,.25;.5,.5;close-button.png;exit;;false;close-button.png]"
-                
-            minetest.after(0.1, function()
-                minetest.show_formspec(player_name, "keyp:login", formspec)
+        if fields.refresh_url then
+            show_loading_formspec(player_name, "loading...")
+            minetest.after(0.25, function()
+                show_initial_login_formspec(player_name)
             end)
+        elseif fields.next then
+            step[player_name] = 2
+            show_code_formspec(player_name)
+        elseif fields.close then
+            minetest.close_formspec(player_name, "keyp:login")
         elseif fields.submit then
-            local formspec = 
-                "formspec_version[4]" ..
-                "size[9,9]" ..
-                "no_prepend[]" ..
-                "background[0,0;9,9;login-modal-75alpha.png;true]" ..
-                "bgcolor[;neither;]" ..
-                "label[4,4;logging in...]" 
-                
-            minetest.show_formspec(player_name, "keyp:login", formspec)
+            show_loading_formspec(player_name, "logging in...")
             local code = fields.code
             local headers = auth_headers
             -- headers["code"] = code
@@ -80,15 +57,21 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
             http_lib.request_http_get(auth_api_url.. "getInfo/".. code, headers, function(response)
                 if response.succeeded then
                     local data = minetest.parse_json(response.data)
+                    
+                    -- Check if there's an error message in the response data
+                    if data and data.message then
+                        -- There's an error message, so handle the error
+                        error_message[player_name] = data.message
+                        show_code_formspec(player_name, error_message[player_name])
+                        return
+                    end
+            
                     if data and data.accessToken and data.walletAddress then
-                        -- Save access token and wallet address
                         player_metadata[player_name].access_token = data.accessToken
                         player_metadata[player_name].wallet_address = data.walletAddress
                         -- Add the HUD element
                         local player = minetest.get_player_by_name(player_name)
-                        print(player)
                         if player then
-                            print("success")
                             add_wallet_address_to_hud(player, data.walletAddress)
                             local formspec = 
                                 "formspec_version[4]" ..
@@ -104,31 +87,89 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
                             -- minetest.show_formspec(player_name, "keyp:login", formspec)
                                 minetest.close_formspec(player_name, "keyp:login")
                             end)
+                            step[player_name] = nil -- Reset the step
+                            error_message[player_name] = nil
                         else
                             print("error", "Player not found!")
                         end
                     else
-                        minetest.chat_send_player(player_name, "Failed to get wallet address.")
+                        error_message[player_name] = "Failed to get wallet address."
+                        show_code_formspec(player_name, error_message[player_name])
                     end
                 else
-                    minetest.chat_send_player(player_name, "Failed to get wallet address.")
+                    error_message[player_name] = "Failed to get wallet address."
+                    show_code_formspec(player_name, error_message[player_name])
                 end
             end)
-        
-            step[player_name] = nil -- Reset the step
         end
-    
     end
 end)
 
-minetest.register_on_leaveplayer(function(player)
-    local player_name = player:get_player_name()
-    step[player_name] = nil -- Reset the step when the player leaves
-end)
+function show_initial_login_formspec(player_name)
+    step[player_name] = 1 
+    local formspec = 
+        "formspec_version[4]" ..
+        "size[9,9]" ..
+        "no_prepend[]" ..
+        "background[0,0;9,9;login-modal-welcome-75alpha.png;true]" ..
+        "bgcolor[;neither;]" ..
+        "textarea[2,4.5;5,.5;url;;"..login_url.."]" ..
+        "image_button[7,4.5;.5,.5;refresh-button.png;refresh_url;;false;refresh-button.png]".. -- The refresh button
+        "image_button[3.72,7.25;1.44,1.12;next-button.png;next;;false;next-button.png]"..
+        "image_button[8,.25;.5,.5;close-button.png;close;;false;close-button.png]"
+    local formname = "keyp:login"
+    minetest.show_formspec(player_name, formname, formspec)
+end
 
+function show_code_formspec(player_name, error_message)
+    local formspec = 
+        "formspec_version[4]" ..
+        "size[9,9]" ..
+        "no_prepend[]" ..
+        "background[0,0;9,9;login-modal-75alpha.png;true]" ..
+        "bgcolor[;neither;]" ..
+        "label[2.55,3;Enter the 6-digit code you received:]" ..
+        "field[3.5,4;1.5,.75;code;Code;]" ..
+        "button[0,0;0,0;submit;]"..
+        "image_button[3.72,7.25;1.44,1.12;next-button.png;submit;;false;next-button.png]"..
+        "image_button[8,.25;.5,.5;close-button.png;close;;false;close-button.png]"
+    
+    -- If there's an error message, add it to the formspec
+    if error_message then
+        formspec = formspec .. "label[2.75,5;".. minetest.formspec_escape(error_message) .."]"
+    end
+
+    minetest.show_formspec(player_name, "keyp:login", formspec)
+end
+
+function show_loading_formspec(player_name, message)
+    local formspec = 
+        "formspec_version[4]" ..
+        "size[9,9]" ..
+        "no_prepend[]" ..
+        "background[0,0;9,9;login-modal-75alpha.png;true]" ..
+        "bgcolor[;neither;]" ..
+        "label[4,4;"..message.."]" 
+                
+    minetest.show_formspec(player_name, "keyp:login", formspec)
+end
+
+minetest.register_chatcommand("login", {
+    description = "Open the login form",
+    func = function(name, param)
+        show_initial_login_formspec(name)
+    end,
+})
+
+minetest.register_chatcommand("logout", {
+    description = "Logout of keyp by deleting access token and wallet address, does not expire access token in your browser",
+    func = function(name, param)
+        logout(name)
+    end, 
+})
 
 function add_wallet_address_to_hud(player, wallet_address)
-    player:hud_add({
+    player_metadata[player:get_player_name()].hud_id = player:hud_add({
         hud_elem_type = "text",
         position = {x = 1, y = 0},
         offset = {x = -475, y = 10},
@@ -137,3 +178,19 @@ function add_wallet_address_to_hud(player, wallet_address)
         number = 0xFFFFFF
     })
 end
+
+function logout(player_name)
+    local player = minetest.get_player_by_name(player_name)
+    if player and player_metadata[player_name] and player_metadata[player_name].hud_id then
+        player:hud_remove(player_metadata[player_name].hud_id)
+    end
+    player_metadata[player_name] = nil
+    minetest.chat_send_player(player_name, "Successfully logged out.")
+end
+
+minetest.register_on_leaveplayer(function(player)
+    local player_name = player:get_player_name()
+    logout(player_name)
+    step[player_name] = nil -- Reset the step when the player leaves
+    error_message[player_name] = nil -- Reset the error message when the player leaves
+end)
